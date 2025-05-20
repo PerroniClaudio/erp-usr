193867e8dfeecf05f873af4a0487fdf2c72dae7a
Claudio Perroni <c.perroni@ifortech.com>
2025-05-20 14:42:12 +0200

    db

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 git config --global --add safe.directory /var/www/erp-usr
-sudo chmod -R 777 storage bootstrap/cache vendor database/database.sqlite
+sudo chmod -R 777 storage bootstrap/cache vendor 
+sudo chmod 777 database/database.sqlite
 
 php artisan migrate --force
 composer install --no-interaction --prefer-dist --optimize-autoloader
@@ -18,4 +19,5 @@ php artisan queue:restart
 npm run build
 
 sudo chown -R www-data:www-data .
-sudo chmod -R 775 storage bootstrap/cache vendor database/database.sqlite
+sudo chmod -R 775 storage bootstrap/cache vendor
+sudo chmod 775 database/database.sqlite
