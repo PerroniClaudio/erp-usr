223f732e05a9f34659c6acb292212ff12f8ea8f3
Claudio Perroni <c.perroni@ifortech.com>
2025-05-20 10:11:45 +0200

    start sh parte 2

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
 sudo chown -R www-data:www-data storage bootstrap/cache
 sudo chmod -R 775 storage bootstrap/cache
+
+php artisan migrate --force
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan queue:restart
